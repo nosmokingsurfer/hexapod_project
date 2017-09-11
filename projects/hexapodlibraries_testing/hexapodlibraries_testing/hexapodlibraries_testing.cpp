21dc545ba47ebf7b349945679b9d4d850b01d268
nosmokingsurfer <nosmokingsurfer@gmail.com>
2017-09-11 23:45:21 +0300

    inverse kinematics fixed

---
+++ projects/hexapodlibraries_testing/hexapodlibraries_testing/hexapodlibraries_testing.cpp
@@ -0,0 +1,41 @@
+// hexapodlibraries_testing.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+ #include <leg.h>
+
+#include <vector>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+  Leg testLeg;
+
+  vector<double> segs;
+  segs.push_back(1);
+  segs.push_back(2);
+  segs.push_back(3);
+
+  Eigen::Vector4d mountingPoint(0,0,0,1);
+  Eigen::Vector3d mountingAngles(0, 0, 0);
+
+
+  testLeg.init(segs, mountingPoint, mountingAngles);
+
+  Vector3d solution;
+  solution.fill(0);
+
+
+
+  Eigen::Vector3d traj = testLeg.trajectoryGenerator(0);
+
+  testLeg.inverseKinematics(traj, solution);
+
+  cout << solution << endl;
+
+	 return 0;
+}
+
