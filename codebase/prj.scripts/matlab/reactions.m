849ed8a16f2067084d17c570ea2939e12fdbc8a2
nosmokingsurfer <nosmokingsurfer@gmail.com>
2018-02-04 21:02:00 +0300

    correct equations of static equilibrium

--- codebase/prj.scripts/matlab/reactions.m
+++ codebase/prj.scripts/matlab/reactions.m
@@ -23,9 +23,9 @@ R_4 = N_4*[ kx_4, ky_4, 1]';
 F_c = [0, 0, -P]';
 
 
-eq1 = R_1(1) + R_2(1) + R_3(1) + R_4(1) == F_c(1);
-eq2 = R_1(2) + R_2(2) + R_3(2) + R_4(2) == F_c(2);
-eq3 = R_1(3) + R_2(3) + R_3(3) + R_4(3) == F_c(3);
+eq1 = R_1(1) + R_2(1) + R_3(1) + R_4(1) + F_c(1) == 0
+eq2 = R_1(2) + R_2(2) + R_3(2) + R_4(2) + F_c(2) == 0
+eq3 = R_1(3) + R_2(3) + R_3(3) + R_4(3) + F_c(3) == 0
 
 mom_R_1 = cross(r_1, R_1);
 mom_R_2 = cross(r_2, R_2);
@@ -33,34 +33,35 @@ mom_R_3 = cross(r_3, R_3);
 mom_R_4 = cross(r_4, R_4);
 mom_F_c = cross(r_c, F_c);
 
-eq4 = mom_R_1(1) + mom_R_2(1) + mom_R_3(1) + mom_R_4(1) == mom_F_c(1);
-eq5 = mom_R_1(2) + mom_R_2(2) + mom_R_3(2) + mom_R_4(2) == mom_F_c(2);
-eq6 = mom_R_1(3) + mom_R_2(3) + mom_R_3(3) + mom_R_4(3) == mom_F_c(3);
-
-eq7 = N_1 > 0;
-eq8 = N_2 > 0;
-eq9 = N_3 > 0;
-eq10 = N_4 > 0;
-eq11 = P > 0;
-eq12 = y_c > 0;
-eq13 = z_c > 0;
-eq14 = h > 0;
-eq15 = l > 0;
-eq16 = alpha >= 0;
-eq17 = y_c < l;
+eq4 = mom_R_1(1) + mom_R_2(1) + mom_R_3(1) + mom_R_4(1) + mom_F_c(1) == 0
+eq5 = mom_R_1(2) + mom_R_2(2) + mom_R_3(2) + mom_R_4(2) + mom_F_c(2) == 0
+eq6 = mom_R_1(3) + mom_R_2(3) + mom_R_3(3) + mom_R_4(3) + mom_F_c(3) == 0
 
 
+eq7 = N_1 > 0
+eq8 = N_2 > 0
+eq9 = N_3 > 0
+eq10 = N_4 > 0
+eq11 = P > 0
+eq12 = y_c > 0
+eq13 = z_c > 0
+eq14 = h > 0
+eq15 = l > 0
+eq16 = alpha >= 0
+eq17 = alpha < pi/2
 
+equations = [eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9, eq10, eq11, eq12, eq13, eq14, eq15]%, eq16, eq17];
 
-equations = [eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9, eq10, eq11, eq12, eq13, eq14, eq15, eq16, eq17];
-
-%equations = subs(equations, alpha, 0);
+disp('Initial equations:')
+for i = 1:6
+    pretty(simplify(equations(i)))    
+end
 
 syms kx real
 syms kz real
 syms ky real
 
-equations = subs(equations, [kx_1, kx_2, kx_3, kx_4],[kx, kx, kx, kx]);
+equations = subs(equations, [kx_1, kx_2, kx_3, kx_4],[kx, -kx, kx, -kx]);
 equations = subs(equations, [kz_1, kz_2], [kz, kz]);
 equations = subs(equations, [ky_3, ky_4], [ky, ky]);
 
@@ -74,26 +75,22 @@ syms N_d real
 equations = subs(equations, [N_1, N_2], [N_u, N_u]);
 equations = subs(equations, [N_3, N_4], [N_d, N_d]);
 
-
+disp('N_1 = N_2 = N_u; N_3 = N_4 = N_d')
 for i = 1:6
     pretty(simplify(equations(i)))    
 end
 
-equations = subs(equations, alpha, 0);
-% 
-% for i = 1:6
-%     pretty(simplify(equations(i)))    
-% end
-
 syms k real
-syms N real
 
-equations = subs(equations, [kx, ky, kz], [0,k,k]);
-%equations = subs(equations, [N_u, N_d], [N, N]);
+equations = subs(equations, [ky, kz], [-k, k]);
+%equations = subs(equations, P, 10);
+%equations = subs(equations, [l, y_c, h], [1, 0.5, 1]);
 
-disp('ololo')
+disp('Final system:')
 for i = 1:6
    pretty(simplify(equations(i)))    
 end
 
-solution = solve(equations,[N_u, N_d, k], 'ReturnConditions', true)
+solution = solve(equations, [N_u, N_d, k], 'ReturnConditions', true)
+solution.conditions
+solution.parameters
