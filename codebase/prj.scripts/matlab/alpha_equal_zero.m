a9753d5f90fa5cb208035649a405d912297b9b0e
nosmokingsurfer <nosmokingsurfer@gmail.com>
2018-02-14 01:07:23 +0300

    solution for right cliff obtained

--- codebase/prj.scripts/matlab/alpha_equal_zero.m
+++ codebase/prj.scripts/matlab/alpha_equal_zero.m
@@ -1,13 +1,28 @@
 syms N_u N_d k P h y_c l real
 
+
+% N_u == N_d kt
+% 
+% 2 N_d + 2 N_u kt == P
+% 
+% 2 N_u h + P y_c == 2 N_d l
+
+
 eqs = [N_u - N_d*k == 0;
        2*N_d + 2*N_u*k == P;
        2*N_u*h + P*y_c == 2*N_d*l;
        P > 0;
        y_c > 0;
-       l > 0;
        h > 0;
-       k > 0];
+       N_u > 0;
+       N_d > 0;
+       y_c < l;
+       k > 0;];
    
-solution =  solve(eqs, [N_u, N_d, k], 'ReturnConditions', true)
+solution =  solve(eqs, [N_u, N_d, k], 'ReturnConditions', true);
+disp('N_u = ')
 pretty(solution.N_u)
+disp('N_d = ')
+pretty(solution.N_d)
+disp('k = ')
+pretty(solution.k)
