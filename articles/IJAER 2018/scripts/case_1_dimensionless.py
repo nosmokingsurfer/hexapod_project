4c80ffee5dc7ad198807e5aef7dfa3eecef6582c
nosmokingsurfer <nosmokingsurfer@gmail.com>
2018-03-11 22:44:42 +0300

    [add] IJAER article template added with some initial text filling

---
+++ articles/IJAER 2018/scripts/case_1_dimensionless.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rc('text', usetex=True)
+
+p1 = np.arange(0, 50, 0.5)
+
+plt.plot(p1, 2 + p1)
+plt.plot(p1, 1 - 0.25*p1**2)
+plt.fill_between(p1, 2+p1, 1-0.25*p1**2)
+
+plt.grid(True)
+plt.xlabel('p_1', fontsize=20)
+plt.ylabel('p_2', fontsize=20)
+plt.title('$0 < k < 1$', fontsize=20)
+plt.xlim((0,30))
+plt.axis('equal')
+plt.show()
