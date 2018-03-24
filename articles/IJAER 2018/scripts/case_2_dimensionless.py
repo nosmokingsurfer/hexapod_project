e58b5d047dcc9795f6d81b0993cecc93ffc449c5
nosmokingsurfer <nosmokingsurfer@gmail.com>
2018-03-24 10:22:49 +0300

    [enh] article is finished

--- articles/IJAER 2018/scripts/case_2_dimensionless.py
+++ articles/IJAER 2018/scripts/case_2_dimensionless.py
@@ -7,7 +7,7 @@ p1 = np.arange(-30, 30, 0.5)
 
 plt.plot(p1, 2 - p1)
 plt.plot(p1, 1 - 0.25*p1**2)
-plt.fill_between(p1, 2-p1, 1-0.25*p1**2)
+plt.fill_between(p1, 2-p1, 1-0.25*p1**2, color='none', hatch='/', edgecolor='blue')
 
 plt.grid(True)
 plt.xlabel('p_1', fontsize=20)
@@ -15,4 +15,7 @@ plt.ylabel('p_2', fontsize=20)
 plt.title('$0 < k < 1$', fontsize=20)
 plt.xlim((0,30))
 plt.axis('equal')
+
+plt.text(-15,0,'$0 < k < 1$', fontsize=30)
+
 plt.show()
