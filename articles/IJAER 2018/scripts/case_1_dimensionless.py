fbd7ac8544540a9583e2aaf24ff315fb01952aba
nosmokingsurfer <nosmokingsurfer@gmail.com>
2018-04-03 19:03:37 +0300

    some mistakes in formulas and figures fixed

--- articles/IJAER 2018/scripts/case_1_dimensionless.py
+++ articles/IJAER 2018/scripts/case_1_dimensionless.py
@@ -3,11 +3,34 @@ import numpy as np
 
 plt.rc('text', usetex=True)
 
-p1 = np.arange(0, 50, 0.5)
 
-plt.plot(p1, 2 + p1)
-plt.plot(p1, 1 - 0.25*p1**2)
-plt.fill_between(p1, 2+p1, 1-0.25*p1**2, color='none',hatch="/", edgecolor='blue')
+
+
+#plt.plot(p1, 1 - 0.25*p1**2)
+
+
+delta = 0.025
+
+p1 = np.arange(0,5,delta)
+p2 = np.arange(1,5,delta)
+
+
+plt.fill_between(p1, 2+p1, 1, color='grey')
+
+
+
+P1, P2 = np.meshgrid(p1,p2)
+
+Z = -0.5*P1 + (0.25*P1**2 + P2 - 1)**0.5
+#Z[Z>1] = 1
+#Z[Z>0] = 0
+
+V = np.arange(0,1.1,0.1)
+
+CS = plt.contour(P1,P2,Z,V, colors='black')
+plt.clabel(CS,CS.levels)
+
+
 
 plt.grid(True)
 plt.xlabel('p_1', fontsize=20)
@@ -16,6 +39,9 @@ plt.title('$0 < k < 1$', fontsize=20)
 plt.xlim((0,30))
 plt.axis('equal')
 
-plt.text(5,4,'$0 < k < 1$', fontsize=30)
+#plt.text(5,4,'$0 < k < 1$', fontsize=30)
 
 plt.show()
+
+
+
--- articles/IJAER 2018/scripts/case_2_dimensionless.py
+++ articles/IJAER 2018/scripts/case_2_dimensionless.py
@@ -3,11 +3,37 @@ import numpy as np
 
 plt.rc('text', usetex=True)
 
-p1 = np.arange(-30, 30, 0.5)
 
-plt.plot(p1, 2 - p1)
-plt.plot(p1, 1 - 0.25*p1**2)
-plt.fill_between(p1, 2-p1, 1-0.25*p1**2, color='none', hatch='/', edgecolor='blue')
+delta = 0.025
+p1 = np.arange(-3, 0, delta)
+p2 = np.arange( 0, 5, delta)
+
+P1, P2 = np.meshgrid(p1,p2)
+
+Z1 = 0.5*P1 + (0.25*P1**2 + P2 - 1)**0.5
+
+V = np.arange(0,1.1,0.1)
+CS = plt.contour(P1,P2,Z1,V,colors='black')
+plt.clabel(CS,CS.levels)
+
+
+p1 = np.arange(0,5,delta)
+p2 = np.arange(-3,0,delta)
+
+P1, P2 = np.meshgrid(p1,p2)
+
+Z1 = 0.5*P1 - (0.25*P1**2 + P2 - 1)**0.5
+
+V = np.arange(0,1.1,0.1)
+CS = plt.contour(P1,P2,Z1,V,colors='black')
+plt.clabel(CS,CS.levels)
+
+#plt.plot(p1, 2 - p1)
+#plt.plot(p1, 1 - 0.25*p1**2)
+p1 = np.arange(2,5,delta)
+plt.fill_between(p1, 2-p1, 0, color='grey')
+p1 = np.arange(-3,0,delta)
+plt.fill_between(p1, 2-p1, 1, color='grey')
 
 plt.grid(True)
 plt.xlabel('p_1', fontsize=20)
@@ -16,6 +42,6 @@ plt.title('$0 < k < 1$', fontsize=20)
 plt.xlim((0,30))
 plt.axis('equal')
 
-plt.text(-15,0,'$0 < k < 1$', fontsize=30)
+#plt.text(0,1.25,'$0 < k < 1$', fontsize=30)
 
 plt.show()
